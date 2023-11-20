5d23db1150279a835bc038816918929a94c7d22c
deusnyakundi <nyakundideus1@gmail.com>
2023-11-20 09:32:01 +0300

    Fixing bugs in the formulae

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,19 @@ function set(){
   $(document).ready(function(){
     $("#quiz").submit(function(event){
       event.preventDefault();
-      var language = parseInt($("input:radio[name=Q1]:checked").val());
-      var logic = parseInt($("input:radio[name=Q2]:checked").val());
-      var method = parseInt($("input:radio[name=Q3]:checked").val());
-      var constant = parseInt($("input:radio[name=Q4]:checked").val());
-      var popup = parseInt($("input:radio[name=Q5]:checked").val());
-      var array = parseInt($("input:radio[name=Q6]:checked").val());
-      var display =parseInt((language + logic + method + constant + popup + array)/100*100);
+      var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
+      var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
+      var Q3 = parseInt($("input:radio[name=Q3]:checked").val());
+      var Q4 = parseInt($("input:radio[name=Q4]:checked").val());
+      var Q5 = parseInt($("input:radio[name=Q5]:checked").val());
+      var Q6 = parseInt($("input:radio[name=Q6]:checked").val());
+      var Q7 = parseInt($("input:radio[name=Q7]:checked").val());
+      var Q8 = parseInt($("input:radio[name=Q8]:checked").val());
+      var Q9 = parseInt($("input:radio[name=Q9]:checked").val());
+      var Q10 = parseInt($("input:radio[name=Q10]:checked").val());
+
+
+      var display =parseInt((Q1+Q2+Q3+Q4+Q5+Q6+Q7+Q8+Q9+Q10));
       var name=$("input#firstName").val();
       $("#hide").slideDown();
       $("input").prop("disabled", true);
